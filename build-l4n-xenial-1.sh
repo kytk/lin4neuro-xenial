75675719403ede8a3bab9aee3949cc28013ac8e1
kytk <kiyotaka@nemotos.net>
2016-12-20 10:59:52 +0900

    add default-jre

--- build-l4n-xenial-1.sh
+++ build-l4n-xenial-1.sh
@@ -62,7 +62,7 @@ sudo apt -y install xfce4 xfce4-terminal xfce4-indicator-plugin 	\
 #Installation of misc packages
 echo "Installation of misc packages"
 sudo apt -y install wajig imagemagick evince vim gedit \
-	unzip zip gparted byobu
+	unzip zip gparted byobu default-jre
 
 #vim settings
 cp /usr/share/vim/vimrc ~/.vimrc
