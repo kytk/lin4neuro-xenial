6d4629151d376b229722989fc4285aa5da13abcf
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-18 01:02:24 +0900

    comment out unnecessary fields

--- build-l4n-xenial-4.sh
+++ build-l4n-xenial-4.sh
@@ -7,12 +7,12 @@ currentdir=`echo $(cd $(dirname $0) && pwd)`
 base_path=$currentdir/lin4neuro-parts
 
 #Add PATH settings to .bashrc
-existrobex=`grep '#ROBEX' ~/.bashrc`
-if [ "$existrobex" != "#ROBEX" ]; then
-    cat ${base_path}/bashrc/bashrc-addition.txt >> $HOME/.bashrc
-else
-    echo ".bashrc setting is already done."
-fi
+#existrobex=`grep '#ROBEX' ~/.bashrc`
+#if [ "$existrobex" != "#ROBEX" ]; then
+#    cat ${base_path}/bashrc/bashrc-addition.txt >> $HOME/.bashrc
+#else
+#    echo ".bashrc setting is already done."
+#fi
 
 #Source .bashrc
 . ~/.bashrc
@@ -20,20 +20,20 @@ fi
 echo "Check if neuroimaging software is properly installed."
 
 #AFNI
-if [ ! -e ~/.afnirc ]; then
-    cp /usr/local/afni/bin/AFNI.afnirc ~/.afnirc
-else
-    echo ".afnirc exists"
-fi
-
-if [ ! -e ~/.sumarc ]; then
-    suma -update_env
-else
-    echo ".sumarc exists"
-fi
-
-afni_system_check.py -check_all
-wait
+#if [ ! -e ~/.afnirc ]; then
+#    cp /usr/local/afni/bin/AFNI.afnirc ~/.afnirc
+#else
+#    echo ".afnirc exists"
+#fi
+#
+#if [ ! -e ~/.sumarc ]; then
+#    suma -update_env
+#else
+#    echo ".sumarc exists"
+#fi
+#
+#afni_system_check.py -check_all
+#wait
 
 #Slicer
 Slicer &
