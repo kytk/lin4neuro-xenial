db66dab3a444981871b03c28a19868488b97d571
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-17 23:25:17 +0900

    add install function of ROBEX and c3d

--- build-l4n-xenial-3.sh
+++ build-l4n-xenial-3.sh
@@ -22,15 +22,49 @@ sudo apt install -y libboost-thread1.58.0 libboost-program-options1.58.0 qt5-def
 #Copy bashcom.sh for c3d to ~/bin
 cp -r ${base_path}/bin $HOME
 
-#Download and extract neuroimaging software packages and tutorial
+#packages to be installed as a default
 
-#3D Slicer
+
+#ROBEX
+cd $HOME/Downloads
+wget http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/ROBEXv12.linux64.tar.gz
+cd /usr/local
+sudo tar xvzf ~/Downloads/ROBEXv12.linux64.tar.gz
+cd ROBEX
+sudo find -type f -exec chmod 644 {} \;
+sudo chmod 755 ROBEX runROBEX.sh dat ref_vols
+
+grep ROBEX ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+    echo "" >> ~/.bashrc
+    echo "#ROBEX" >> ~/.bashrc
+    echo "export PATH=$PATH:/usr/local/ROBEX" >> ~/.bashrc
+fi
+
 #c3d
+cd $HOME/Downloads
+wget http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/c3d-1.0.0-Linux-x86_64.tar.gz
+cd /usr/local
+sudo tar xvzf ~/Downloads/c3d-1.0.0-Linux-x86_64.tar.gz
+sudo mv c3d-1.0.0-Linux-x86_64 c3d
+
+grep c3d ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+    echo "" >> ~/.bashrc
+    echo "#c3d" >> ~/.bashrc
+    echo "export PATH=$PATH:/usr/local/c3d" >> ~/.bashrc
+fi
+
+#Mango
+#MRIcron
+#MRIcroGL
+#tutorial
+
+#packages to be installed by users (with installer)
+#3D Slicer
 #ANTs
 #itksnap
-#MRIcroGL
-#MRIcron
-#ROBEX
-#tutorial
+#CONN17f
+#SPM12 standalone
 
 exit
