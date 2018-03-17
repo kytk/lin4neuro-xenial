d3aa133b73a5b494b8222bbb20fef22c4e7434ab
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-18 01:01:53 +0900

    add scripts for installation

--- build-l4n-xenial-3.sh
+++ build-l4n-xenial-3.sh
@@ -24,6 +24,7 @@ cp -r ${base_path}/bin $HOME
 
 #packages to be installed as a default
 
+#3D Slicer
 
 #ROBEX
 cd $HOME/Downloads
@@ -53,17 +54,71 @@ if [ $? -eq 1 ]; then
     echo "" >> ~/.bashrc
     echo "#c3d" >> ~/.bashrc
     echo "export PATH=$PATH:/usr/local/c3d" >> ~/.bashrc
+    echo "source $HOME/bin/bashcomp.sh" >> ~/.bashrc
 fi
 
+#itksnap
+cd $HOME/Downloads
+wget http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/itksnap-3.6.0-20170401-Linux-x86_64.tar.gz
+cd /usr/local
+sudo tar xvzf ~/Downloads/itksnap-3.6.0-20170401-Linux-x86_64.tar.gz
+sudo mv itksnap-3.6.0-20170401-Linux-x86_64 itksnap
+
+grep itksnap ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+    echo "" >> ~/.bashrc
+    echo "#ITK-SNAP" >> ~/.bashrc
+    echo "export PATH=$PATH:/usr/local/itksnap/bin" >> ~/.bashrc
+fi
+
+
 #Mango
+cd $HOME/Downloads
+wget http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/mango_unix.zip
+cd /usr/local
+sudo tar xvzf ~/Downloads/mango_unix.zip
+
 #MRIcron
+cd $HOME/Downloads
+wget http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/lx.zip
+cd /usr/local
+sudo unzip ~/Downloads/lx.zip
+sudo mv mricron_lx mricron
+cd mricron
+sudo find lut -type f -exec chmod 644 {} \;
+sudo find templates -type f -exec chmod 644 {} \;
+
+grep mricron ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+    echo "" >> ~/.bashrc
+    echo "#MRIcron" >> ~/.bashrc
+    echo "export PATH=$PATH:/usr/local/mricron" >> ~/.bashrc
+fi
+
+
 #MRIcroGL
+cd $HOME/Downloads
+wget http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/mricrogl_linux.zip
+cd /usr/local
+sudo unzip ~/Downloads/mricrogl_linux.zip
+sudo mv mricrogl_lx mricrogl
+cd mricrogl
+sudo rm .DS_Store
+sudo find -type f -exec chmod 644 {} \;
+sudo find -type d -exec chmod 755 {} \;
+sudo chmod 755 MRIcroGL dcm2niix pigz_mricron
+
+grep mricrogl ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+    echo "" >> ~/.bashrc
+    echo "#MRIcroGL" >> ~/.bashrc
+    echo "export PATH=$PATH:/usr/local/mricrogl" >> ~/.bashrc
+fi
+
 #tutorial
 
 #packages to be installed by users (with installer)
-#3D Slicer
 #ANTs
-#itksnap
 #CONN17f
 #SPM12 standalone
 
