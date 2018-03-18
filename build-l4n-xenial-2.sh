892795de2e163daba3d099e638e9d7a07bcecfb7
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-18 14:32:58 +0900

    bugfix

--- build-l4n-xenial-2.sh
+++ build-l4n-xenial-2.sh
@@ -12,9 +12,10 @@ base_path=$currentdir/lin4neuro-parts
 
 #Install R
 sudo apt-get install -y software-properties-common
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD
-57CBB651716619E084DAB9
-sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+sudo apt-key adv --keyserver keyserver.ubuntu.com \
+     --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository \
+ 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
 sudo apt-get -y update
 sudo apt-get install -y r-base
 
