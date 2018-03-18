6a19543598eb7bc60219341cf0c6238b21357f19
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-19 01:00:35 +0900

    add ANTs installer

---
+++ installer/ANTs_installer_ubuntu1604.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Install ANTs"
+cd $HOME/Downloads
+
+if [ ! -e 'ANTs.zip' ]; then
+  curl -O http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/ANTs.zip
+fi
+
+cd /usr/local
+sudo unzip ~/Downloads/ANTs.zip
+
+grep ANTs ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+    echo '' >> ~/.bashrc
+    echo '#ANTs' >> ~/.bashrc
+    echo 'export ANTSPATH=/usr/local/ANTs/bin' >> ~/.bashrc
+    echo 'export PATH=$PATH:$ANTSPATH' >> ~/.bashrc
+fi
+
+
