13191d6443850df44d575f2a4828f4956e8e7a91
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-19 00:38:35 +0900

    add spm12_standalone installer

---
+++ installer/spm12_installer_ubuntu1604.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SPM12 standalone installer
+
+#Check MCR is installed
+if [ ! -d /usr/local/MATLAB/MCR/v92 ]; then
+  echo "Matlab Compiler Runtime needs to be installed first!"
+  mcr_v92_installer_ubuntu1604.sh
+fi
+
+#Download SPM12 standalone
+echo "Download SPM12 standalone"
+cd $HOME/Downloads
+
+if [ ! -e 'spm12_standalone.zip' ]; then
+  curl -O http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/spm12_standalone.zip
+fi
+
+cd /usr/local
+sudo unzip ~/Downloads/spm12_standalone.zip
+
+echo "Initialize SPM12 standalone"
+echo "Press Quit when SPM12 is up"
+sleep 5
+sudo /usr/local/spm12_standalone/run_spm12.sh /usr/local/MATLAB/MCR/v92
+
+exit
+
