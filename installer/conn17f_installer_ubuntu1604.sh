d1c5feda531b5f38fd04e8e55f18a681380dfdc8
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-19 00:55:19 +0900

    add conn installer

---
+++ installer/conn17f_installer_ubuntu1604.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#CONN17f standalone installer
+
+#Check MCR is installed
+if [ ! -d /usr/local/MATLAB/MCR/v92 ]; then
+  echo "Matlab Compiler Runtime needs to be installed first!"
+  mcr_v92_installer_ubuntu1604.sh
+fi
+
+#Download SPM12 standalone
+echo "Download CONN17f standalone"
+cd $HOME/Downloads
+
+if [ ! -e 'conn17f_glnxa64.zip' ]; then
+  curl -O http://www.lin4neuro.net/lin4neuro/neuroimaging_software_packages/conn17f_glnxa64.zip
+fi
+
+cd /usr/local
+sudo unzip ~/Downloads/conn17f_glnxa64.zip
+
+sed -i 's/NoDisplay=true/NoDisplay=false/' ~/.local/share/applications/conn.desktop
+
+echo "Finished! Run CONN from menu -> Neuroimaging -> CONN"
+exit
+
