85a9fc9726729acc7181a069efa96a5d56f24665
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-31 09:31:51 +0900

    update build-l4n-xenial-3.sh

--- build-l4n-xenial-3.sh
+++ build-l4n-xenial-3.sh
@@ -13,13 +13,28 @@ echo "Run Slicer"
 Slicer &
 wait
 
+#Aliza
+echo "Run Aliza"
+aliza &
+wait
+
 #c3d
 echo "Run c3d"
 c3d -h
 
+#DSI Studio
+echo "Run DSI-Studio"
+/usr/local/dsistudio/dsi_studio &
+wait
+
 #ITK-SNAP
 echo "Run itksnap"
 itksnap &
+wait
+
+#Mango
+echo "Run Mango"
+/usr/local/Mango/mango &
 wait
 
 #MRIcroGL
@@ -36,6 +51,12 @@ wait
 #ROBEX
 echo "Run ROBEX"
 ROBEX
+
+#Virtual MRI
+echo "Virtual MRI"
+virtual-mri &
+wait
+
 
 echo "Finish checking!"
 
