768ddcc3537059897e485489236f4180e881bcf6
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-04-21 17:33:52 +0900

    add virtualbox_guest installer with shared folder configuration

--- installer-scripts/virtualbox_guest_xenial.sh
+++ installer-scripts/virtualbox_guest_xenial.sh
@@ -4,6 +4,17 @@
 sudo apt-get -y install virtualbox-guest-dkms
 sudo usermod -aG vboxsf $(whoami)
 
+#prepare shared directory
+if [ ! -e /media/sf_share ]; then
+  echo "prepare shared directory"
+  sudo mkdir /meida/sf_share
+fi
+
+#/etc/fstab
+echo '' | sudo tee -a /etc/fstab
+echo '#Virtualbox shared folder' | sudo tee -a /etc/fstab
+echo 'share   /media/sf_share vboxsf    uid=1000,gid=1000    0    0' | sudo tee -a /etc/fstab
+
 echo "Reboot system in 5 seconds"
 sleep 5
 sudo reboot
