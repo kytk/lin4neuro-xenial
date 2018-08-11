23e893322781e244af930c7c848204ddc08a88e6
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-08-11 13:16:57 +0900

    comment out @add_rcran_ubuntu.tcsh

--- installer-scripts/afni_installer_xenial_2.sh
+++ installer-scripts/afni_installer_xenial_2.sh
@@ -13,8 +13,8 @@ cd $HOME
 export R_LIBS=$HOME/R
 mkdir $R_LIBS
 echo 'export R_LIBS=$HOME/R' >> ~/.bashrc
-curl -O https://afni.nimh.nih.gov/pub/dist/src/scripts_src/@add_rcran_ubuntu.tcsh
-sudo tcsh @add_rcran_ubuntu.tcsh
+#curl -O https://afni.nimh.nih.gov/pub/dist/src/scripts_src/@add_rcran_ubuntu.tcsh
+#sudo tcsh @add_rcran_ubuntu.tcsh
 rPkgsInstall -pkgs ALL
 
 #Make AFNI/SUMA profiles
