e4276644c47d9ea4a7fc068cbbe9d8abae8a202d
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2020-01-27 23:41:38 +0900

    update spm12 installer

--- installer-scripts/spm12_installer_xenial.sh
+++ installer-scripts/spm12_installer_xenial.sh
@@ -2,9 +2,9 @@
 #SPM12 standalone installer
 
 #Check MCR is installed
-if [ ! -d /usr/local/MATLAB/MCR/v92 ]; then
+if [ ! -d /usr/local/MATLAB/MCR/v95 ]; then
   echo "Matlab Compiler Runtime needs to be installed first!"
-  ~/git/lin4neuro-xenial/installer-scripts/mcr_v92_installer_xenial.sh
+  ~/git/lin4neuro-xenial/installer-scripts/mcr_v95_installer_xenial.sh
 fi
 
 #Download SPM12 standalone
@@ -23,12 +23,12 @@ sed -i 's/NoDisplay=true/NoDisplay=false/' ~/.local/share/applications/spm.deskt
 #alias 
 echo '' >> ~/.bashrc
 echo '#SPM12 standalone' >> ~/.bashrc
-echo "alias spm='/usr/local/spm12_standalone/run_spm12.sh /usr/local/MATLAB/MCR/v92'" >> ~/.bashrc
+echo "alias spm='/usr/local/spm12_standalone/run_spm12.sh /usr/local/MATLAB/MCR/v95'" >> ~/.bashrc
 
 echo "Initialize SPM12 standalone"
 echo "Press Quit when SPM12 is up"
 sleep 5
-sudo /usr/local/spm12_standalone/run_spm12.sh /usr/local/MATLAB/MCR/v92 ; \
+sudo /usr/local/spm12_standalone/run_spm12.sh /usr/local/MATLAB/MCR/v95 ; \
 sudo chown -R $(whoami):$(whoami) ~/.matlab
 
 exit
